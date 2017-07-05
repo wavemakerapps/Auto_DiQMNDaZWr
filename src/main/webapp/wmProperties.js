607e7366c30f5d94d135c5a6f1a05e5d51197aab
root <root@61949b3d2f95>
2017-07-05 00:33:21 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "default",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "Auto_DiQMNDaZWr",
+  "homePage" : "Main",
+  "name" : "Auto_DiQMNDaZWr",
+  "platformType" : "DEFAULT",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "PREFAB",
+  "version" : "1.0"
+};
